9d302ac77cc116bfd4d1ac5b6c1a568b46f720ed
Anthony Osigbe <thronesofglory@gmail.com>
2023-10-27 07:49:37 -0700

    11. Zombie tasks completed

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+ * infinite_while - Run an endless while loop.
+ *
+ * This function runs a continuous while loop,
+ * without an exit condition.
+ *
+ * Return: Always returns 0.
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - Spawns five zombie processes.
+ *
+ * This function creates five zombie processes through forking.
+ *
+ * Return: Always returns 0.
+ */
+
+int main(void)
+{
+	int i;
+	pid_t child_pid;
+
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
+
+		if (child_pid == 0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+			exit(0);
+		}
+	}
+	infinite_while();
+	return (0);
+}
