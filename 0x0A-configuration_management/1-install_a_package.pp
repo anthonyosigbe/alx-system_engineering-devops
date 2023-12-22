810dc8571e6c397052134e4ccb446b04d2870ec3
Anthony Osigbe <thronesofglory@gmail.com>
2023-12-22 05:35:35 -0800

    1. Install a package tasks completed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
 # Install flask package from pip3 using Puppet
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_python3_pip':
+  command => '/usr/bin/apt-get update && /usr/bin/apt-get install -y python3-pip',
+  unless  => '/usr/bin/dpkg -l | grep -q "python3-pip"',
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  require => Exec['install_python3_pip'],
+}
+
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug==2.0.2',
+  unless  => '/usr/bin/pip3 show Werkzeug | grep -q "Version: 2.0.2"',
+  require => Exec['install_flask'],
 }
