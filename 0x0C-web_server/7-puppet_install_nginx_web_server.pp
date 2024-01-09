a5389e4a98130e162fc5492d824fa38fa4efffc5
Anthony Osigbe <thronesofglory@gmail.com>
2024-01-09 03:33:36 -0800

    file update and bug fix

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -6,39 +6,22 @@
 # (requesting a page) using curl, it must return
 # a page that contains the string Hello World!
 
-include stdlib
-
-$link = 'http://example.com/'
-$content = "\trewrite ^/redirect_me/$ ${link} permanent;"
-
-exec { 'update packages':
-  command => '/usr/bin/apt-get update'
-}
-
-exec { 'restart nginx':
-  command => '/usr/sbin/service nginx restart',
-  require => Package['nginx']
-}
-
 package { 'nginx':
-  ensure  => 'installed',
-  require => Exec['update packages']
+  ensure => installed,
+}
+
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/anthonyosigbe permanent;',
 }
 
 file { '/var/www/html/index.html':
-  ensure  => 'present',
-  content => 'Hello World',
-  mode    => '0644',
-  owner   => 'root',
-  group   => 'root'
+  content => 'Hello World!',
 }
 
-file_line { 'Set 301 redirection':
-  ensure   => 'present',
-  after    => 'server_name\ _;',
-  path     => '/etc/nginx/sites-available/default',
-  multiple => true,
-  line     => $content,
-  notify   => Exec['restart nginx'],
-  require  => File['/var/www/html/index.html']
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
