481b33d63b7b946e0c762bf2e2451b4a0dafb076
Anthony Osigbe <thronesofglory@gmail.com>
2024-02-13 04:20:23 -0800

    updates

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,6 +2,7 @@
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
