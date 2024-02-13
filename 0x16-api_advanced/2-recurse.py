21ad9f8e2484689be721678be7fdeb369a8281ca
Anthony Osigbe <thronesofglory@gmail.com>
2024-02-13 05:22:29 -0800

    2. Recurse it! tasks completed

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
+"""
+Utilizing the API of Reddit
+"""
 import requests
+after = None
 
 
-def recurse(subreddit, after='', hot_list=[]):
-    # Base URL for fetching hot articles from a subreddit
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=25'
-    if after:
-        url += f'&after={after}'
+def recurse(subreddit, hot_list=[]):
+    """Recursively fetching the top ten post titles"""
+    global after
+    user_agent = {'User-Agent': 'api_advanced-project'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    results = requests.get(url, params=parameters, headers=user_agent,
+                           allow_redirects=False)
 
-    headers = {'User-Agent': 'api_advanced-project'}
-    # Make the request to the Reddit API
-    response = requests.get(url, headers=headers)
-    # Check if the subreddit is valid and not a redirect (status code 200)
-    if response.status_code != 200:
-        return None
-    # Load the JSON response
-    data = response.json()
-    # Extract the list of articles
-    articles = data.get('data', {}).get('children', [])
-    if not articles:
-        return hot_list if hot_list else None
-    # Add the titles of the current page of articles to the hot_list
-    hot_list.extend([article['data']['title'] for article in articles])
-    # Check if there is a next page
-    after = data.get('data', {}).get('after', '')
-    if not after:
+    if results.status_code == 200:
+        after_data = results.json().get("data").get("after")
+        if after_data is not None:
+            after = after_data
+            recurse(subreddit, hot_list)
+        all_titles = results.json().get("data").get("children")
+        for title_ in all_titles:
+            hot_list.append(title_.get("data").get("title"))
         return hot_list
     else:
-        # Recursively call the function to get articles from the next page
-        return recurse(subreddit, after, hot_list)
+        return (None)
