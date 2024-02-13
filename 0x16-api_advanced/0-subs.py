aec2c429d44f69f60a27f5aa607107ef8cca0273
Anthony Osigbe <thronesofglory@gmail.com>
2024-02-13 04:18:38 -0800

    updates

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""Fetches subreddit subscriber count."""
-
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Returns subscriber count for a specified subreddit or
-    0 if subreddit is invalid."""
+    """Return the total number of subscribers on a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.ok:
-        return response.json().get("data", {}).get("subscribers", 0)
-    return 0
+    headers = {"User-Agent": "anthonyosigbe"}
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            results = response.json().get("data", {})
+            return results.get("subscribers", 0)
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
